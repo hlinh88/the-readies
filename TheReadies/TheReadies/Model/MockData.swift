f7ea12430175a5600ccf70ce0e44e7d1be44f945
Luke Nguyen <luke@outcubator.com>
2025-02-11 16:45:47 +0700

    Update Home

---
+++ TheReadies/TheReadies/Model/MockData.swift
@@ -0,0 +1,30 @@
+//
+//  MockData.swift
+//  TheReadies
+//
+//  Created by Luke Nguyen on 10/2/25.
+//
+
+struct Book: Codable {
+    var id: Int
+    var image: String
+    var title: String
+    var author: String
+    
+    init(image: String, title: String, author: String) {
+        self.image = image
+        self.title = title
+        self.id = title.hashValue
+        self.author = author
+    }
+}
+
+let READING_NOW_BOOKS: [Book] = [.init(
+    image: "",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald"
+), .init(
+    image: "",
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee"
+)]
--- TheReadies/TheReadies/Scene/Home/HomeView.swift
+++ TheReadies/TheReadies/Scene/Home/HomeView.swift
@@ -31,11 +31,13 @@ struct HomeView: View {
                 Text("The Readies")
                     .font(.headline)
                     .frame(maxWidth: .infinity)
+                    .opacity(0)
             }
             
             ScrollView {
                 LazyVStack(spacing: 16) {
                     Text("The Readies")
+                        .bold()
                         .font(.largeTitle)
                         .frame(maxWidth: .infinity, alignment: .leading)
                     
@@ -43,44 +45,17 @@ struct HomeView: View {
                         Section {
                             switch section {
                             case .ReadingNow:
-                                ScrollView(.horizontal) {
-                                    LazyHStack(spacing: 16) {
-                                        HStack {
-                                            Image(.bookCover)
-                                                .resizable()
-                                                .frame(width: 60, height: 80)
-                                            
-                                            VStack(alignment: .leading){
-                                                Text("The Great Gatsby")
-                                                    .font(.headline)
-                                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                                
-                                                Text("F. Scott Fitzgerald")
-                                                    .font(.caption)
-                                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                                
-                                                ProgressView(value: 0.5, total: 1.0)
-                                                    .progressViewStyle(.linear)
-                                                    .tint(.white)
-                                            }
-                                            .frame(maxWidth: .infinity)
-                                            
-                                            Spacer()
-                                        }
-                                        .frame(width: 250, height: 100)
-                                        .padding(.horizontal, 16)
-                                        .background(
-                                            LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing)
-                                                .cornerRadius(12)
-                                        )
-                                    }
-                                }
+                                ReadingNowView(books: READING_NOW_BOOKS)
+                                
+                            case .Library:
+                                LibraryView()
                                 
                             default: EmptyView()
                             }
                         } header: {
                             Text(section.title)
-                                .font(.headline)
+                                .font(.system(size: 20))
+                                .fontWeight(.semibold)
                                 .frame(maxWidth: .infinity, alignment: .leading)
                         }
 
@@ -92,6 +67,100 @@ struct HomeView: View {
         }
         .padding(.horizontal, 16)
 //        .ignoresSafeArea(edges: .top)
+    }
+    
+    struct ReadingNowView: View {
+        let books: [Book]
+        
+        var body: some View {
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 16) {
+                    ForEach(books, id: \.id) { book in
+                        HStack {
+                            Image(.bookCover)
+                                .resizable()
+                                .frame(width: 60, height: 80)
+                            
+                            VStack(alignment: .leading){
+                                Text(book.title)
+                                    .font(.headline)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                
+                                Text(book.author)
+                                    .font(.caption)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                
+                                ProgressView(value: 0.5, total: 1.0)
+                                    .progressViewStyle(.linear)
+                                    .tint(.white)
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                            Spacer()
+                        }
+                        .frame(width: 250, height: 120)
+                        .padding(.horizontal, 16)
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing)
+                                .cornerRadius(20)
+                        )
+                    }
+                }
+            }
+        }
+    }
+    
+    struct LibraryView: View {
+        var body: some View {
+            VStack(spacing: 16) {
+                HStack {
+                    Image(systemName: "books.vertical.fill")
+                    Text("Library")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("3")
+                    Image(systemName: "chevron.right")
+                }
+                
+                HStack {
+                    Image(systemName: "book.fill")
+                    Text("Reading")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("3")
+                    Image(systemName: "chevron.right")
+                }
+                
+                HStack {
+                    Image(systemName: "bookmark.fill")
+                    Text("To Read")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("3")
+                    Image(systemName: "chevron.right")
+                }
+                
+                HStack {
+                    Image(systemName: "checkmark.seal.fill")
+                    Text("Finished")
+                        .font(.headline)
+                    
+                    Spacer()
+                    
+                    Text("3")
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .cornerRadius(20)
+            .padding(16)
+            .background(Color.gray.opacity(0.1))
+        }
     }
 }
 
